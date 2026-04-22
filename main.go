4ab65742de1261dcc9a64c68dc9fb77a4b17eae5
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2026-04-22 09:13:57 +0200

    feature: add initial time sheets for testing

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"github.com/das-kaesebrot/timesheet/internal/model"
 	"github.com/das-kaesebrot/timesheet/internal/password"
@@ -26,5 +28,14 @@ func main() {
 	if err != nil {
 		panic("Failed to hash password!")
 	}
-	err = gorm.G[model.User](db).Create(ctx, &model.User{Username: "admin", PasswordHash: pwHash, Active: false, WeeklyWorkHours: 40})
+
+	description := "getting some work done"
+	err = gorm.G[model.User](db).Create(ctx, &model.User{Username: "admin", PasswordHash: pwHash, Active: true, WeeklyWorkHours: 40})
+	user, err := gorm.G[model.User](db).Where("username = ?", "admin").First(ctx)
+	err = gorm.G[model.TimesheetEntry](db).Create(ctx, &model.TimesheetEntry{UserID: user.ID, Start: time.Now(), End: time.Now().Add(time.Hour), Description: &description})
+	err = gorm.G[model.TimesheetEntry](db).Create(ctx, &model.TimesheetEntry{UserID: user.ID, Start: time.Now().Add(time.Hour), End: time.Now().Add(time.Hour), Description: &description})
+	err = gorm.G[model.TimesheetEntry](db).Create(ctx, &model.TimesheetEntry{UserID: user.ID, Start: time.Now(), End: time.Now().Add(time.Hour), Description: &description})
+
+	fmt.Printf("Found user: %v", user)
+	fmt.Printf("Found time entries: %v", user.TimesheetEntries)
 }
