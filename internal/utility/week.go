437163ee2ec4d3bd3e9c5d3a875af1b7bb5cce2c
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2026-05-13 21:19:49 +0200

    add method for zeroing time components

--- internal/utility/week.go
+++ internal/utility/week.go
@@ -11,7 +11,7 @@ func GenerateWeekRanges(start time.Time, end time.Time, weekStartDay time.Weekda
 // going from the given start time (or rather, the corresponding date), find and return the start of the next week, aligned to the given week start day.
 // if weekStartDay is the same day, the same day is returned
 func GetNextWeekStartDate(start time.Time, weekStartDay time.Weekday) time.Time {
-	start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+	start = ZeroTimeComponents(start)
 
 	delta := int((weekStartDay - start.Weekday() + 7) % 7)
 	start = start.AddDate(0, 0, delta)
@@ -20,10 +20,15 @@ func GetNextWeekStartDate(start time.Time, weekStartDay time.Weekday) time.Time 
 }
 
 func GetPreviousWeekStartDate(start time.Time, weekStartDay time.Weekday) time.Time {
-	start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+	start = ZeroTimeComponents(start)
 
 	delta := int((start.Weekday() - weekStartDay + 7) % 7)
 	start = start.AddDate(0, 0, -delta)
 
 	return start
+}
+
+// returns a new date from the given date that has the hours, minutes, seconds and nanoseconds components zeroed.
+func ZeroTimeComponents(toZero time.Time) time.Time {
+	return time.Date(toZero.Year(), toZero.Month(), toZero.Day(), 0, 0, 0, 0, toZero.Location())
 }
