b103a5905abef0cdbf90c4ec7b65b050573fe1f2
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2026-05-16 00:04:13 +0200

    feature: add automatic color mode and dark/light mode toggler

---
+++ web/static/js/script.js
@@ -0,0 +1,31 @@
+!function () {
+    var t = localStorage.getItem("theme");
+    if (!t) {
+        t = window.matchMedia("(prefers-color-scheme:dark)").matches ? "dark" : "light";
+    }
+    document.documentElement.setAttribute("data-bs-theme", t);
+}()
+
+// dark and light mode toggler
+!function () {
+    var key = 'theme';
+    var stored = localStorage.getItem(key);
+    
+    // https://stackoverflow.com/a/57795495
+    var prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    var theme = stored || (prefersDark ? 'dark' : 'light');
+    document.documentElement.setAttribute('data-bs-theme', theme);
+
+    var toggle = document.getElementById('themeToggle');
+    var icon = document.getElementById('themeIcon');
+    if (toggle) {
+        icon.textContent = theme === 'dark' ? '☀️' : '🌙';
+        toggle.addEventListener('click', function () {
+            var current = document.documentElement.getAttribute('data-bs-theme');
+            var next = current === 'dark' ? 'light' : 'dark';
+            document.documentElement.setAttribute('data-bs-theme', next);
+            localStorage.setItem(key, next);
+            icon.textContent = next === 'dark' ? '☀️' : '🌙';
+        });
+    }
+}()
