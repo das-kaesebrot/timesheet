6b60c17c117deeafc0e2a7d4216b7c18535b3dbf
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2026-07-31 23:10:28 +0200

    fix: flashing color on initial page load with dark theme

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,20 +1,6 @@
-!function () {
-    var t = localStorage.getItem("theme");
-    if (!t) {
-        t = window.matchMedia("(prefers-color-scheme:dark)").matches ? "dark" : "light";
-    }
-    document.documentElement.setAttribute("data-bs-theme", t);
-}()
-
 // dark and light mode toggler
 !function () {
-    var key = 'theme';
-    var stored = localStorage.getItem(key);
-    
-    // https://stackoverflow.com/a/57795495
-    var prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    var theme = stored || (prefersDark ? 'dark' : 'light');
-    document.documentElement.setAttribute('data-bs-theme', theme);
+    var theme = document.documentElement.getAttribute('data-bs-theme');
 
     var toggle = document.getElementById('themeToggle');
     var icon = document.getElementById('themeIcon');
@@ -24,7 +10,7 @@
             var current = document.documentElement.getAttribute('data-bs-theme');
             var next = current === 'dark' ? 'light' : 'dark';
             document.documentElement.setAttribute('data-bs-theme', next);
-            localStorage.setItem(key, next);
+            localStorage.setItem('theme', next);
             icon.textContent = next === 'dark' ? '☀️' : '🌖';
         });
     }
